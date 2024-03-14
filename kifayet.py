5920816bd86b13562ff708f1020379260a5514a8
Berat Cabuk <102920898+beratcabuk@users.noreply.github.com>
2024-03-14 19:32:19 +0100

    updates to the api key intake

--- kifayet.py
+++ kifayet.py
@@ -4,23 +4,28 @@ from driver import GoogleShopping
 
 
 def main():
-    if len(sys.argv) == 1:
-        print("Please supply an image file and your API key.\nTo get help, try: kifayet -h")
-    elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
-        print("usage: kifayet filename")
+    if sys.argv[1] == "-h" or sys.argv[1] == "--help":
+        print("usage: kifayet img_file apikey_file")
         sys.exit(0)
+    elif len(sys.argv) != 3:
+        print("Please supply an image file and a file with the API key.\nTo get help, try: kifayet -h")
     else:
         # Check if filename points to an image.
         if sys.argv[1][-4:] not in {'.png', '.jpg'}:
             print('The file must be of a png/jpg image!')
             sys.exit(1)
 
-        file_name, api_key = sys.argv[1], sys.argv[2]
+        img_file_name, api_key_file_name = sys.argv[1], sys.argv[2]
+        # Read the api key from the indicated file
+        with open(api_key_file_name, 'r') as f:
+            api_key = f.read()
+            f.close()
+
         bot = Bot(api_key)
         gs = GoogleShopping()
 
         # Encode the image to base64
-        encoded_image = bot.encode_image(file_name)
+        encoded_image = bot.encode_image(img_file_name)
         # Send the image to openai, get the keywords
         keywords = bot.respond(encoded_image)
         # Get the links that match to the keywords
